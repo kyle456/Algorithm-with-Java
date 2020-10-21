d6f3e9d6afe0b31d5f109c1b82bb72579b8831e8
kyle456 <kyle2123@naver.com>
2020-10-21 18:54:58 +0900

    No1080_행렬 개선된 풀이

--- src/baekjoon/greedy/No1080_행렬.java
+++ src/baekjoon/greedy/No1080_행렬.java
@@ -13,8 +13,8 @@ public class No1080_행렬 {
 		int m = scanner.nextInt(); // 열의 개수
 		scanner.nextLine();
 
-		int[][] matrixA = new int[n][m];
-		int[][] matrixB = new int[n][m];
+		int[][] matrixA = new int[n][m]; // 행렬A
+		int[][] matrixB = new int[n][m]; // 행렬B
 
 		// 행렬A
 		for (int i = 0; i < n; ++i) {
@@ -36,56 +36,34 @@ public class No1080_행렬 {
 			}
 		}
 
-		if (n < 3 || m < 3) {
-			if (!Arrays.deepEquals(matrixA, matrixB)) {
-				System.out.println(-1);
-			} else {
-				System.out.println(0);
-			}
-
-			return;
-		}
-
 		int count = 0;
-		int rowIndex = 0;
-		int colIndex = 0;
 
-		while (!Arrays.deepEquals(matrixA, matrixB)) {
-			if (matrixA[rowIndex][colIndex] != matrixB[rowIndex][colIndex]) {
-				for (int i = rowIndex; i < rowIndex + 3; ++i) {
-					for (int j = colIndex; j < colIndex + 3; ++j) {
-						if (matrixA[i][j] == 0)
-							matrixA[i][j] = 1;
-						else
-							matrixA[i][j] = 0;
-					}
+		// 3 x 3 연산의 가용 범위까지 연산 (그리디 하게, 행렬A와 행렬B가 다른 원소가 존재하기만 하면 바로 연산)
+		for (int i = 0; i <= n - 3; ++i) {
+			for (int j = 0; j <= m - 3; ++j) {
+				if (matrixA[i][j] != matrixB[i][j]) {
+					trans(matrixA, i, j);
+					count++;
 				}
-
-				count++;
-			}
-			
-			if (colIndex + 3 < m) {
-				colIndex++;
-			} else {
-				if (matrixA[rowIndex][colIndex + 1] != matrixB[rowIndex][colIndex + 1]
-						|| matrixA[rowIndex][colIndex + 2] != matrixB[rowIndex][colIndex + 2]) {
-					System.out.println(-1);
-					return;
-				}
-				colIndex = 0;
-				rowIndex++;
-				
-				if (rowIndex + 2 >= n) {
-					if (!Arrays.deepEquals(matrixA, matrixB)) {
-						count = -1;
-					}
-					break;
-				}
-				
 			}
 		}
 
-		System.out.println(count);
+		if (Arrays.deepEquals(matrixA, matrixB)) {
+			System.out.println(count);
+		} else {
+			System.out.println(-1);
+		}
+
+	}
+
+	// 3 x 3 범위 만큼 0 -> 1 , 1 -> 0 으로 바꿔주는 함수
+	public static void trans(int[][] matrixA, int row, int column) {
+		for (int i = row; i < row + 3; ++i) {
+			for (int j = column; j < column + 3; ++j) {
+				// matrixA[i][j]가 0이면 1로, 1이면 0으로 (^ 연산자 사용)
+				matrixA[i][j] ^= 1;
+			}
+		}
 
 	}
 

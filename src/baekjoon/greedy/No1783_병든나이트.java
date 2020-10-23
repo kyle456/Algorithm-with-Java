2156fef480148619e3eab2a7a5bbf6e5a46f32d4
kyle456 <kyle2123@naver.com>
2020-10-23 11:51:54 +0900

    No1783_병든나이트 임시저장

---
+++ src/baekjoon/greedy/No1783_병든나이트.java
@@ -0,0 +1,18 @@
+package baekjoon.greedy;
+
+import java.util.Scanner;
+
+public class No1783_병든나이트 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int n = scanner.nextInt(); // 체스판의 세로 길이
+		int m = scanner.nextInt(); // 체스판의 가로 길이		
+		int[][] checkerBoard = new int[n][m]; // 체스판
+		
+		
+
+	}
+
+}

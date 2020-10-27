7f356fbd515fd6ff5d2b85cad241d34cf7dcc187
kyle456 <kyle2123@naver.com>
2020-10-27 15:42:48 +0900

    No5_문자열재정렬 풀이

---
+++ src/thisiscodingtest/implementation/No5_문자열재정렬.java
@@ -0,0 +1,30 @@
+package thisiscodingtest.implementation;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class No5_문자열재정렬 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String s = scanner.next();
+		String[] sSplit = s.split("");
+		int sum = 0;
+		
+		Arrays.sort(sSplit);
+		
+		for (String i : sSplit) {
+			char eachOne = i.charAt(0);
+			
+			if (eachOne >= 48 && eachOne <= 57) {
+				sum += eachOne - '0';
+			} else {
+				System.out.print(eachOne);
+			}
+		}
+		
+		System.out.println(sum);
+	}
+
+}

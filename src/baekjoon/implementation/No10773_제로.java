bc7cd1b4b8cdd11b8640e7fed5ad5a9dac45d9ec
kyle456 <kyle2123@naver.com>
2020-10-29 21:47:23 +0900

    No10773_제로 : Arraylist + BufferedReader 사용풀이

--- src/baekjoon/implementation/No10773_제로.java
+++ src/baekjoon/implementation/No10773_제로.java
@@ -1,35 +1,40 @@
 package baekjoon.implementation;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class No10773_제로 {
 
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int k = scanner.nextInt(); // 입력할 정수의 갯수
-		int[] numbers = new int[k]; // 재민이가 부르는 수
-		int sum = 0;
-
+		int k = Integer.parseInt(br.readLine()); // 입력받을 정수의 갯수
+		ArrayList<Integer> numbers = new ArrayList<Integer>(); // 재민이가 부르는 수
+		int temp = 0; // 임시 저장
+		int sum = 0; // 합계
+		
 		for (int i = 0; i < k; i++) {
-			numbers[i] = scanner.nextInt();
-
-			if (numbers[i] == 0) {
-				for (int j = i - 1; j >= 0; j--) {
-					if (numbers[j] != 0) {
-						numbers[j] = 0;
-						break;
-					}
-				}
+			temp = Integer.parseInt(br.readLine()); 
+			
+			// 0이 아니면 numbers에 저장
+			if (temp != 0) {
+				numbers.add(temp);
+			} 
+			// 0이면 numbers의 맨 끝에 있는 정수 삭제
+			else {
+				numbers.remove(numbers.size() - 1);
 			}
 		}
-
-		for (int i = 0; i < k; i++) {
-			sum += numbers[i];
+		
+		br.close();
+		
+		for (int i = 0; i < numbers.size(); i++) {
+			sum += numbers.get(i);
 		}
-
+		
 		System.out.println(sum);
-
 	}
 
 }

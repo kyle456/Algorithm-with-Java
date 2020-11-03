ebc00f649da83ad3b078021fda43cd1a0b4816e8
kyle456 <kyle2123@naver.com>
2020-11-03 15:31:26 +0900

    No1009_분산처리 효율적 풀이

---
+++ bin/README.md
@@ -0,0 +1,10 @@
+﻿# 알고리즘 문제 풀이
+* Baekjoon  
+&nbsp; 1) 그리디(Greedy)  
+&nbsp; 2) 구현(Implementation)  
+---
+* CodeUp  
+&nbsp;1) 기초 100제(Basic 100)  
+---
+* This is Coding Test (이것이 취업을 위한 코딩테스트다.)  
+&nbsp;1) 그리디(Greedy)   
--- src/baekjoon/implementation/No1009_분산처리.java
+++ src/baekjoon/implementation/No1009_분산처리.java
@@ -6,28 +6,50 @@ import java.io.InputStreamReader;
 import java.math.BigInteger;
 
 public class No1009_분산처리 {
-
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BigInteger tenNum = new BigInteger("10"); // 마지막 처리될 컴퓨터 번호를 알기 위해 나눌 10
-				
+
 		int t = Integer.parseInt(br.readLine()); // 테스트 케이스의 수
-		
+		int[] result = new int[t]; // 결과값
+
 		for (int i = 0; i < t; i++) {
-			String[] abSplit = br.readLine().split(" "); // 정수 a b
-			
-			BigInteger numberA = new BigInteger(abSplit[0]);
-			BigInteger numberPow = numberA.pow(Integer.parseInt(abSplit[1])); // a의 b제곱		
-			int computer = numberPow.remainder(tenNum).intValue(); // 10으로 나눈 나머지
-			
+			String[] integerAB = br.readLine().split(" ");
+			int a = Integer.parseInt(integerAB[0]); // 정수 a
+			int b = Integer.parseInt(integerAB[1]); // 정수 b
+			int computer = newPow(a, b);
+
 			if (computer == 0) {
-				System.out.println(10); // 나머지가 0이면 10번 컴퓨터로 처리
+				result[i] = 10;
 			} else {
-				System.out.println(computer); // 나머지 번 컴퓨터로 처리
+				result[i] = computer;
+			}
+		}
+
+		for (int i : result) {
+			System.out.println(i);
+		}
+
+		br.close();
+	}
+	
+	// a의 b제곱 값의 1의 자리 반환
+	public static int newPow(int a, int b) {
+		int temp = a;
+		
+		for (int i = 1; i < b; i++) {
+			a *= temp;
+			
+			// 10이상일 경우 1자리 수로 만듦
+			if (a >= 10) {
+				a %= 10;
 			}
 		}
 		
-		br.close();
+		if (a >= 10) {
+			a %= 10;
+		}
+
+		return a;
 	}
 
 }

6afdd2aa6164baddb2dd3f4485c9226d2d72e118
kyle456 <kyle2123@naver.com>
2020-11-04 18:27:24 +0900

    programmers 큰 수 만들기 효율적 풀이

--- src/programmers/큰수만들기.java
+++ src/programmers/큰수만들기.java
@@ -1,46 +1,37 @@
 package programmers;
 
-import java.util.ArrayList;
-
 public class 큰수만들기 {
-	public String solution(String number, int k) {
-		// String to Integer
-		ArrayList<Integer> intNum = new ArrayList<Integer>(number.length());
-		for (int i = 0; i < number.length(); i++) {
-			intNum.add(number.charAt(i) - '0');
-		}
-
-		int thisIndex = 0;
-		int nextIndex = 1;
-		while (k > 0) {
-			// 뒤 숫자가 크면 앞 숫자 지우고, 다시 처음부터 검사
-			if (intNum.get(thisIndex) < intNum.get(nextIndex)) {
-				intNum.remove(thisIndex);
-				k--;
-				thisIndex = 0;
-				nextIndex = 1;
-				continue;
-			}
-			// 뒤 숫자가 작으면 지우지 않고 다음 index로 넘어감
-			else {
-				thisIndex++;
-				nextIndex++;
-				if (nextIndex > intNum.size() - 1) {
-					nextIndex = 0;
-				}
-				if (thisIndex > intNum.size() - 1) {
-					thisIndex = 0;
-				}
-			}
-		}
-
-		// Integer to String
-		String answer = "";
-		for (int i = 0; i < intNum.size(); i++) {
-			answer += Integer.toString(intNum.get(i));
-		}
-
-		return answer;
-	}
-
+    public String solution(String number, int k) {        
+        String answer = "";
+        StringBuilder sbNum = new StringBuilder(number);
+        
+        int thisIndex = 0;
+        int nextIndex = 1;
+        int lastIndex;
+        while(k > 0) {
+            // 뒤 숫자가 크면 앞 숫자 지우고, 다시 처음부터 검사
+            if (sbNum.charAt(thisIndex) < sbNum.charAt(nextIndex)) {
+                sbNum.deleteCharAt(thisIndex);
+                k--;
+                thisIndex = 0;
+                nextIndex = 1;
+            } 
+            // 뒤 숫자가 작으면 지우지 않고 다음 index로 넘어감
+            else {
+                thisIndex++;
+                nextIndex++;
+                lastIndex = sbNum.length() - 1;
+                if (nextIndex > lastIndex) {
+                    nextIndex = 0;
+                }
+                if (thisIndex > lastIndex) {
+                    thisIndex = 0;
+                }
+            }
+        }        
+        
+        answer = sbNum.toString();
+        
+        return answer;
+    }
 }

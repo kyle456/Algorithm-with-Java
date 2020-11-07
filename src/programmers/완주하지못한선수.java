b2409303b809690d4e1f704f8c732ee8cf5008a4
kyle456 <kyle2123@naver.com>
2020-11-07 21:08:41 +0900

    programmers 완주하지못한선수 풀이

---
+++ src/programmers/완주하지못한선수.java
@@ -0,0 +1,21 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class 완주하지못한선수 {
+	public String solution(String[] participant, String[] completion) {
+		String answer = "";
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		for (int i = 0; i < completion.length; i++) {
+			if (!participant[i].equals(completion[i])) {
+				answer = participant[i];
+				break;
+			}
+		}
+		if (answer.equals("")) {
+			answer = participant[completion.length];
+		}
+		return answer;
+	}
+}

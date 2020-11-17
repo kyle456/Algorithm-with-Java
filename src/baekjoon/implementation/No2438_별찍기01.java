bb03b32cde19b1712a3fe70e7f368ca7362164e4
kyle456 <kyle2123@naver.com>
2020-11-17 23:18:35 +0900

    No2438_별찍기01 풀이

---
+++ src/baekjoon/implementation/No2438_별찍기01.java
@@ -0,0 +1,20 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+public class No2438_별찍기01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+	}
+
+}

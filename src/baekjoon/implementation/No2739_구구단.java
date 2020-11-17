599a5b4980bd5030ed4ed0b48da95ca24a5ed142
kyle456 <kyle2123@naver.com>
2020-11-17 23:22:58 +0900

    No2739_구구단 풀이

---
+++ src/baekjoon/implementation/No2739_구구단.java
@@ -0,0 +1,17 @@
+package baekjoon.implementation;
+
+import java.util.Scanner;
+
+public class No2739_구구단 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(n + " * " + i + " = " + (n * i));
+		}
+
+	}
+
+}
